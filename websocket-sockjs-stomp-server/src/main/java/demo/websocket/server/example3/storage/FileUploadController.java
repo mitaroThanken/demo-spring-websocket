5776ec6c53b9d11e0eb5ff702a1d8d41b3bde27e
YAMAMI Taro <mitarothanken@gmail.com>
2024-02-10 12:09:49 +0000

    Add Uploading files sample
    https://spring.io/guides/gs/uploading-files

---
+++ websocket-sockjs-stomp-server/src/main/java/demo/websocket/server/example3/storage/FileUploadController.java
@@ -0,0 +1,52 @@
+package demo.websocket.server.example3.storage;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/files")
+public class FileUploadController {
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/")
+    public Stream<String> listUploadFiles() throws IOException {
+        return storageService.loadAll().map((path) -> path.getFileName().toString());
+    }
+
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        storageService.store(file);
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ websocket-sockjs-stomp-server/src/main/java/demo/websocket/server/example3/storage/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package demo.websocket.server.example3.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+    
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
